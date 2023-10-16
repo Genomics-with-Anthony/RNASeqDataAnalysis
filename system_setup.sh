5aa4588730ea715e86a67d81085b3b8ddd126238
Anthony Shoukat <147436198+Genomics-with-Anthony@users.noreply.github.com>
2023-10-16 08:23:26 +0200

    Update system_setup.sh

--- system_setup.sh
+++ system_setup.sh
@@ -2,14 +2,16 @@
 
 # This file will provide all the commands required to prepare system for RNASeq Data Analysis workflow
 
-# The first step is to set up virtual machines due to limited individual computational power.  
+# The first step is to set up virtual machines due to limited individual computational power of local maschine.  
 # The virtual machine’s connection information is provided IP(10.20.102.4 is hypothetical IP) and key(filename.txt)
 
 ssh ubuntu@10.20.102.4 -i pathtofile/filename.txt
 
-# The key for VM’s connection didn’t work due to permission restrictions. The chmod 600 command used for file permission.
+# The key for VM’s connection will not work due to permission restrictions. The chmod 600 command can be used for file permission.
+# chmod is command for permission changing. The number 6 means user got read and write permission but other have no permission (--rw------).
+# File path given at the end of command.
+
 chmod 600 pathtofile/filename.txt
-# chmod is command for permission changing. The number 6 means user got read and write permission but other have no permission (--rw------). File path given at the end of command.
 
 # The second step of system set up will be to install conda. To download directly from websites, wget command followed by URL will used. 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
@@ -18,7 +20,8 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
 bash Miniconda3-py39_4.12.0-Linux-x86_64.sh
 
 # The following command will install conda-forge as well as bioconda channels.
-conda install scipy --channel conda-forge --channel bioconda
+conda install scipy --channel 
+conda-forge --channel bioconda
 
 conda create --name bio # create environment bio
 conda activate bio # activate bio
@@ -41,11 +44,11 @@ conda install -c bioconda fastqc
 
 # Installing flexbar  and flexbar is only available on Ubuntu 
 conda install -c bioconda flexbar
-flexbar -h # open flexbar help to test the flexbar installation
+flexbar -h # open flexbar help to test the flexbar installation and to check available commands
 
 # Installing cutadapt  if you are not working on Ubuntu
 conda install -c bioconda cutadapt
-cutadapt -h # open cutadapt help to test the cutadapt installation
+cutadapt -h # open cutadapt help to test the cutadapt installation and to check available commands
 
 # create new environment mapping and also install star 
 conda create -n mapping star
@@ -55,11 +58,11 @@ conda activate mapping # activate mapping environment
 
 # Installing Picard
 conda install -c bioconda picard
-picard -h # open picard help to test the picard installation
+picard -h # open picard help to test the picard installation and to check available commands
 
 # Installing Bamtools
 conda install -c bioconda bamtools
-bamtools -h # open bamtools help to test the bamtools installation
+bamtools -h # open bamtools help to test the bamtools installation and to check available commands
 
 # create new environment featurecounts and also install featureCounts 
 # subread is conda package which includes featureCounts program and following coomand can be used to install it
